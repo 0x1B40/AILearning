aba9f09e3149a24005073104a9222a595828903e
Ali Al Saif <alialsaif97@gmail.com>
2021-05-15 14:14:27 +0300

    This is the typical data structure for search problems involving trees and nodes

---
+++ Chapter 2/SearchDataStructures.py
@@ -0,0 +1,11 @@
+# common data structure for any search problem involving nodes and trees
+class Node:
+   def __Init__(self,nodeState,nodeParent,nodeAction,nodePathCost):
+       # stores the state of the node, state is dependant on the problem
+       self.nodeState = nodeState
+       # stores the parent node class object
+       self.nodeParent = nodeParent
+       # string to store the type of action taken to transition to another state
+       self.nodeAction = nodeAction
+       # cost of reaching the current node
+       self.nodePathCost = nodePathCost
