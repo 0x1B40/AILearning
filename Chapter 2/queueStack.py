9c7988adc6ac333813dc4c015ebb1bbecb4c7293
Ali Al Saif <alialsaif97@gmail.com>
2021-05-15 15:33:14 +0300

    Native implementation of stack concept in python, significant for the second data structure needed for trees and nodes

---
+++ Chapter 2/queueStack.py
@@ -0,0 +1,18 @@
+
+# declare a list:
+nodeList = []
+# to declare a node
+# node = Node(param1,param2,param3,param4)
+
+# to add a node to the stack
+nodeList.push()
+
+# to check if the stack is empty
+
+nodeList.empty()
+
+# to return the top-most element without removing it
+nodeList.top()
+
+# to return and remove the top-most element
+nodeList.pop()
